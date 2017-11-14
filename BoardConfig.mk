b43d2c778fa8f54ff02690bbcd3cc4aa3f667336
KK666666 <3317717293@qq.com>
2017-11-14 10:54:21 +0800

    cm-14.1(C105)

---
+++ BoardConfig.mk
@@ -0,0 +1,58 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# inherit from msm8996-common
+-include device/leeco/msm8996-common/BoardConfigCommon.mk
+
+TARGET_OTA_ASSERT_DEVICE := ares,le_ares,cp_ares
+
+DEVICE_PATH := device/leeco/ares
+
+TARGET_SPECIFIC_HEADER_PATH += $(DEVICE_PATH)/include
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff loglevel=7
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET     := 0x01000000
+
+TARGET_KERNEL_CONFIG := lineage_ares_defconfig
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# NFC
+TARGET_USES_NQ_NFC := true
+BOARD_NFC_CHIPSET := pn548
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4294967296
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 26144878592
+
+# CM Hardware
+BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/cmhw
+
+# SELinux
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
+# inherit from the proprietary version
+-include vendor/leeco/ares/BoardConfigVendor.mk
---
+++ lineage.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from oneplus3 device
+$(call inherit-product, device/leeco/ares/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_ares
+PRODUCT_DEVICE := ares
+PRODUCT_MANUFACTURER := Coolpad
+PRODUCT_BRAND := Coolpad
+
+PRODUCT_GMS_CLIENTID_BASE := android-leeco
+
+TARGET_VENDOR_PRODUCT_NAME := Ares
+TARGET_VENDOR_DEVICE_NAME := le_ares
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=le_ares PRODUCT_NAME=Ares
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=LeEco/ares_NA/le_ares:6.0.1/WEXNAOP5802101261S/letv01261206:user/release-keys \
+    PRIVATE_BUILD_DESC="le_ares-user 6.0.1 WEXNAOP5802101261S eng.letv.20170126.120318 release-keys"
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
+
+TARGET_VENDOR := leeco
